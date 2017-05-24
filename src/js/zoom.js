a51d29da2f1580168fff4ba86b1d371106c2b82e
Алексей Лапунов < >
2017-05-24 16:00:45 +0300

    basekit

---
+++ src/js/zoom.js
@@ -0,0 +1,14 @@
+function resizeWindow() {
+    var windowWidth = document.documentElement.clientWidth;
+
+    if (windowWidth > 600) {
+        $('.information-picture').zoom({
+            url: '../images/product-4-zoom.jpg'
+        });
+    } else {
+        $('.information-picture').trigger('zoom.destroy');
+    }
+}
+
+window.addEventListener('resize', resizeWindow);
+resizeWindow();
